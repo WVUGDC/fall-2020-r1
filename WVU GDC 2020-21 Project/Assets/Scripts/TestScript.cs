f8342073a5ab8169d357b4a424cdfd05d9a8b779
nsk0007 <60013564+nsk0007@users.noreply.github.com>
2020-09-25 19:28:30 -0400

    Added Unity Project
    Unity Project

---
+++ WVU GDC 2020-21 Project/Assets/Scripts/TestScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Code put here will happen once
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Code put here will happen every frame
+        transform.position = new Vector2(Mathf.Cos(Time.time), Mathf.Abs(Mathf.Sin(Time.time * 5)) * 2);
+    }
+}
