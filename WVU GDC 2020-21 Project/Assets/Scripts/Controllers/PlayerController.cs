71b4fff87664b3b2b423fb340d8d0ecbcca92f9c
AronWinski <al0043@mix.wvu.edu>
2020-10-16 23:59:47 -0400

    Character Controller and Animations
    Added character sprite animations and a character controller and some other folders for organization.

---
+++ WVU GDC 2020-21 Project/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //holds the player's ID
+    //public int playerId;
+
+  
+    
+    [Header("Character Attributes:")]
+    //base speed of character
+    public float MOVEMENT_BASE_SPEED = 1.0f;
+
+    [Space]
+    [Header("Character Statistics:")]
+    //speed of character
+    public float movementSpeed;
+    //holds the direction of the character
+    public Vector2 movementDirection;
+
+    [Space]
+    [Header("References:")]
+    //basic physics for the character
+    public Rigidbody2D rb;
+    //Animation for character
+    public Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+        Move();
+        Animate();
+    }
+
+    void ProcessInputs()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        //locks the speed between two points
+        movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
+
+        //makes sure movementDirection is a unit vector
+        movementDirection.Normalize();
+    }
+
+    void Move()
+    {
+        rb.velocity = movementDirection * movementSpeed * MOVEMENT_BASE_SPEED;
+    }
+
+    void Animate()
+    {
+        animator.SetFloat("Horizontal", movementDirection.x);
+        animator.SetFloat("Vertical", movementDirection.y);
+        animator.SetFloat("Speed", movementSpeed);
+    }
+}
