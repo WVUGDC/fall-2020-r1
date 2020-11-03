a4c414830805ff5c1139a792b5b10988537166bf
nsk0007 <60013564+nsk0007@users.noreply.github.com>
2020-11-02 19:14:38 -0500

    Started weapon

---
+++ WVU GDC 2020-21 Project/Assets/Weapon.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon", order = 1)]
+public class Weapon : ScriptableObject
+{
+    public GameObject bulletPrefab;
+    public float fireSpeed;
+    public float f;
+}
