039aa9dc6b4441968ebfff429b5dd5ea86531035
AronWinski <al0043@mix.wvu.edu>
2020-11-18 01:12:35 -0500

    Made an Audio Manager + button select sound
    Audio Manager is meant to be carried into all scenes and holds an array of all the sounds we want in our game. Currently there is an issue with gameobjects losing their reference to Audio Manager when using an EventTrigger component.  It occurs if you start in the title screen, go to game, and then back to title screen. Once you are back in title screen, the reference to the Audio Manager game object is lost.

---
+++ WVU GDC 2020-21 Project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    //public static AudioManager instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+        //makes sure there is only one instance of this gameObject
+                                                               /*
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        //ensures the game object persists between scenes
+        DontDestroyOnLoad(gameObject);                      */
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    //can use this for continual sounds, music etc.
+    void Start()
+    {
+        
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+}
